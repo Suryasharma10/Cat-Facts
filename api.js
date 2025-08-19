d382322c16a126fb334ff5ee6ecfab42bc62b293
Surya Sharma <suryasharma73077@gmail.com>
2025-08-19 21:12:01 +0530

    Add files via upload

--- api.js
+++ api.js
@@ -1,14 +1,17 @@
+
 const para =  document.querySelector("#fact")
-const newfact = document.querySelector("#btn")
+const newfact = document.querySelector(".btn")
 console.log("api...");
 const URL ="https://meowfacts.herokuapp.com/";
-const getImage = async ()=>{
+para.innerText = "To Know Cat Facts!Click on Get Fact";
+newfact.addEventListener("click",getImage = async ()=>{
     //console.log("getting data...");
     let response = await fetch(URL);
+    para.innerText = response;
+    //console.log(response);
     //console.log(response);
     let image =  await response.json();
     para.innerText=image.data
     //console.log(para.innerText=image.data);
-} 
-getImage()
-newfact.addEventListener("click",getImage);
+});
+
